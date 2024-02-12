24a664de71fe7e091f91da93687aacd553f2f0bb
Matrat-Nayati <nayati.matrat@gmail.com>
2024-02-12 10:56:52 +0100

    PREMIERE VERSION FONCTIONELLE DU SERVER

---
+++ B2L/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Salut j'ai mis 40 années lumières a fonctionner mais je marche</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
