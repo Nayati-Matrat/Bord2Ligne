b943080172451e59157c362fbd59c776f297afe4
MatisMissana <m.missana.sn@gmail.com>
2024-02-26 15:29:31 +0100

    Create settings.py

---
+++ WikiPoisson/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Bord2Ligne',
+        'USER': 'root',
+        'PASSWORD': 'Wamp64',
+        'HOST': 'localhost',  # ou votre hôte MariaDB
+        'PORT': '3306',  # ou le port de votre serveur MariaDB
+    }
+}
