be94b76943cb5bbf3a13222b264770579efa14b9
MatisMissana <m.missana.sn@gmail.com>
2024-03-04 15:20:13 +0100

    Create settings.py

---
+++ WikiPoisson/Wikipoisson_BackEnd/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Bord2Ligne',
+        'USER': 'root',
+        'PASSWORD': 'Wamp65',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
