7265e5eb1efdd65d53eec229c903adbdf8b8b9ff
MatisMissana <m.missana.sn@gmail.com>
2024-04-12 10:11:07 +0200

    Update wikiController.php

--- src/Controller/wiki/wikiController.php
+++ src/Controller/wiki/wikiController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\wiki;
 
+use App\Entity\PoissonAbledeheckel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 /**
  * @Route("/wiki", name="wiki_")
@@ -15,10 +18,16 @@ class wikiController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+
+        
+
+        $articles = $articleRepository->findAll();
+        
         return $this->render('wiki/index.html.twig', [
             'controller_name' => 'wikiController',
+            'articles'=> $articles
         ]);
     }
 }
