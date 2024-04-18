5d328fb8308e6018e2c751a19a6af8a9dbd8050c
MatisMissana <m.missana.sn@gmail.com>
2024-04-18 11:28:44 +0200

    Update wikiController.php

--- src/Controller/wiki/wikiController.php
+++ src/Controller/wiki/wikiController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PoissonAbledeheckelRepository;
+use App\Repository\PoissonSpirlinRepository;
 
 /**
  * @Route("/wiki", name="wiki_")
@@ -16,14 +17,19 @@ class wikiController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(PoissonAbledeheckelRepository $repository): Response
+    public function index(PoissonAbledeheckelRepository $repository,PoissonSpirlinRepository $repository2): Response
 {
-    $test = $repository->findAll();
-    dd($test);
+    $Poissonhabledeheckel = $repository->findAll();
+    $PoissonSpirlin = $repository2->findAll();
 
     
         return $this->render('wiki/index.html.twig', [
             'controller_name' => 'wikiController',
+            'poissonabledeheckel'=>$Poissonhabledeheckel,
+            'poissonspirlin'=> $PoissonSpirlin
         ]);
+
     }
+
+    
 }
