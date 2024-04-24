e916692dfe7c35503967019906deb1dd5a1aaa30
Florent <florent.roch@hotmail.fr>
2024-04-24 10:22:08 +0200

    Update map.js

--- public/js/home/map.js
+++ public/js/home/map.js
@@ -10,33 +10,35 @@ document.addEventListener('DOMContentLoaded', function() {
     // Tableau pour stocker les marqueurs
     var markers = [];
 
-    // Ajouter un marqueur
-    function addMarker(e) {
-        var marker = L.marker(e.latlng).addTo(mymap);
+    // Fonction pour ajouter un marqueur
+    function addMarker(lat, lng, name) {
+        var marker = L.marker([lat, lng]).addTo(mymap);
+        marker.bindPopup(name).openPopup();
+        
         markers.push(marker);
         
-        // Demander un nom pour le marqueur
-        var markerName = prompt('Entrez un nom pour ce marqueur:');
-        
-        // Ajouter le nom comme propriété au marqueur
-        marker.bindPopup(markerName).openPopup();
-        
-        // Stocker le marqueur avec son nom
-        localStorage.setItem(markerName, JSON.stringify(e.latlng));
+        // Ajouter le bouton de suppression
+        var popupContent = '<div>' + name + '</div><button onclick="deleteMarker(' + markers.length + ')">Supprimer</button>';
+        marker.bindPopup(popupContent);
+    }
+
+    // Fonction pour supprimer un marqueur
+    function deleteMarker(index) {
+        if (markers[index - 1]) {
+            mymap.removeLayer(markers[index - 1]);
+            markers.splice(index - 1, 1);
+        }
     }
 
     // Écouter le clic sur la carte pour ajouter un marqueur
-    mymap.on('click', addMarker);
-
-    // Charger les marqueurs depuis localStorage
-    for (var i = 0; i < localStorage.length; i++) {
-        var key = localStorage.key(i);
-        var markerPosition = JSON.parse(localStorage.getItem(key));
+    mymap.on('click', function(event) {
+        var lat = event.latlng.lat;
+        var lng = event.latlng.lng;
         
-        if (markerPosition) {
-            var marker = L.marker(markerPosition).addTo(mymap);
-            marker.bindPopup(key).openPopup();
-            markers.push(marker);
+        var name = prompt("Nom du lieu :");
+        
+        if (name) {
+            addMarker(lat, lng, name);
         }
-    }
+    });
 });
