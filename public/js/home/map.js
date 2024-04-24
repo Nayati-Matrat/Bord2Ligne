1d5a1a90ba0ad1f5f8e57aba931153c22deed45a
Florent <florent.roch@hotmail.fr>
2024-04-24 10:26:42 +0200

    Update map.js

--- public/js/home/map.js
+++ public/js/home/map.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.log('je teste le bon chargement de la page');
-    
+
     var mymap = L.map('map').setView([43.2327, 0.0802], 13); // Coordonnées de Tarbes
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,35 +10,38 @@ document.addEventListener('DOMContentLoaded', function() {
     // Tableau pour stocker les marqueurs
     var markers = [];
 
-    // Fonction pour ajouter un marqueur
-    function addMarker(lat, lng, name) {
-        var marker = L.marker([lat, lng]).addTo(mymap);
-        marker.bindPopup(name).openPopup();
-        
+    // Ajouter un marqueur
+    function addMarker(e) {
+        var marker = L.marker(e.latlng).addTo(mymap);
         markers.push(marker);
-        
-        // Ajouter le bouton de suppression
-        var popupContent = '<div>' + name + '</div><button onclick="deleteMarker(' + markers.length + ')">Supprimer</button>';
-        marker.bindPopup(popupContent);
-    }
 
-    // Fonction pour supprimer un marqueur
-    function deleteMarker(index) {
-        if (markers[index - 1]) {
-            mymap.removeLayer(markers[index - 1]);
-            markers.splice(index - 1, 1);
-        }
+        // Demander un nom pour le marqueur
+        var markerName = prompt('Entrez un nom pour ce marqueur:');
+
+        // Ajouter le nom comme propriété au marqueur
+        marker.bindPopup(markerName).openPopup();
+
+        // Stocker le marqueur avec son nom
+        localStorage.setItem(markerName, JSON.stringify(e.latlng));
     }
 
     // Écouter le clic sur la carte pour ajouter un marqueur
-    mymap.on('click', function(event) {
-        var lat = event.latlng.lat;
-        var lng = event.latlng.lng;
-        
-        var name = prompt("Nom du lieu :");
-        
-        if (name) {
-            addMarker(lat, lng, name);
+    mymap.on('click', addMarker);
+
+    // Charger les marqueurs depuis localStorage
+    for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var markerPositionString = localStorage.getItem(key);
+
+        try {
+            var markerPosition = JSON.parse(markerPositionString);
+            if (markerPosition) {
+                var marker = L.marker(markerPosition).addTo(mymap);
+                marker.bindPopup(key).openPopup();
+                markers.push(marker);
+            }
+        } catch (error) {
+            console.error('Erreur lors de la lecture depuis localStorage :', error);
         }
-    });
+    }
 });
