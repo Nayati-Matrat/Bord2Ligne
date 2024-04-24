66b3ab6a630b140f99b0f3e5782c595de4db7936
MatisMissana <m.missana.sn@gmail.com>
2024-04-24 11:26:45 +0200

    Create Version20240424090815.php

---
+++ migrations/Version20240424090815.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240424090815 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE test ADD id INT AUTO_INCREMENT NOT NULL, ADD PRIMARY KEY (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE test MODIFY id INT NOT NULL');
+        $this->addSql('DROP INDEX `primary` ON test');
+        $this->addSql('ALTER TABLE test DROP id');
+    }
+}
